49a9b93ec41b3cedb3e288f3c07c8b27647a933f
Marcos Padilha <marcos.padilha@paipe.co>
2022-04-26 13:07:07 -0300

    retornar erros

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,3 +1,10 @@
 module.exports.somar = (n1, n2) => {
-  return n1 + n2;
+  const num1 = parseInt(n1);
+  const num2 = parseInt(n2);
+
+  if (Number.isNaN(num1) || Number.isNaN(num2)) {
+    throw new Error("Verifique os números informados");
+  }
+
+  return num1 + num2;
 };
--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -1,5 +1,27 @@
 const { somar } = require("./calculator");
 
 it("deve somar 2 + 2 e o resultado deve ser 4", () => {
-  expect(somar(2, 2)).toBe(4);
+  expect(somar("2", 2)).toBe(4);
+});
+
+it("deve somar 2 + 2 e, mesmo que um deles seja uma string, o resultado deve ser 4", () => {
+  expect(somar("2", 2)).toBe(4);
+});
+
+it("deve mostrar um erro se o que for passado para o método não puder ser somado", () => {
+  expect(() => {
+    somar("", "1");
+  }).toThrowError();
+
+  expect(() => {
+    somar([2, 2]);
+  }).toThrowError();
+
+  expect(() => {
+    somar({});
+  }).toThrowError();
+
+  expect(() => {
+    somar();
+  }).toThrowError();
 });
